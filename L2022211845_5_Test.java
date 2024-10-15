299590c802785f319edfb6c869b3a2796250b1fc
ldk <869989451@qq.com>
2024-10-15 20:23:05 +0800

    提交测试5的修改和单元测试

---
+++ L2022211845_5_Test.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+public class L2022211845_5_Test {
+    @Test //测试方法
+    public void testBinarySearch() {
+    Solution5 solution5 = new Solution5();
+    int[] nums={3,3,6};
+    int target=6;
+    int low = solution5.binarySearch(nums, target);
+    System.out.println(low);
+}
+    @Test
+    public void testPretreatment(){
+        Solution5 solution5 = new Solution5();
+        solution5.pretreatment();
+        int[] f=new int[100];
+        System.out.println(f[0]);
+    }
+
+    @Test
+    public  void  testNumSubseq(){
+        Solution5 solution5 = new Solution5();
+        int[] nums={2,3,3};
+        int target=12;
+        int ans = solution5.numSubseq(nums, target);
+        System.out.println(ans);
+    }
+}
